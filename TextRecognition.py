02c3f740e0c01a96c7ddc5dc51a37566fc854f84
esong200 <e.song200@gmail.com>
2019-02-26 21:33:46 -0800

    New Text regognition using google APIs

--- TextRecognition.py
+++ TextRecognition.py
@@ -14,3 +14,24 @@ class Analyzer:
         self.infile = infile
         self.outfile = outfile
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = googleAPIDirectory
+
+    def get_sentiment(self, content):
+        client = language_v1.LanguageServiceClient()
+        if isinstance(content, six.binary_type):
+            content = content.decode('utf-8')
+
+        type_ = enums.Document.Type.PLAIN_TEXT
+        document = {'type': type_, 'content': content}
+
+        response = client.analyze_sentiment(document)
+        sentiment = response.document_sentiment
+        returnValue = []
+        returnValue.append(sentiment.score)
+        returnValue.append(sentiment.magnitude)
+        # print(returnValue)
+        return returnValue
+
+if __name__ == '__main__':
+    agent = Analyzer(sys.argv[1], sys.argv[2])
+    print(agent.get_sentiment('This computer is amazing. Overall, the processor is fast and the gpu is very powerfull, however, the screen is kind of small'))
+
